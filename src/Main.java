827a5ea5209a97441dab2d5283ed571b87878c5f
clari62 <clariespertino62@gmail.com>
2022-09-26 21:56:46 -0300

    Modificación en rama release2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Modificación en rama develop");
+		System.out.println("Modificación en rama release2");
 	}
 
 }
